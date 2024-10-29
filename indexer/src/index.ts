de4bf6ab3e65660b2e10d59a5188d919d5eb9074
iamnas <iamnas.eth@gmail.com>
2024-10-29 18:35:33 +0530

    added latestblock store

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -6,12 +6,10 @@ import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const providerBNB = new JsonRpcProvider(process.env.BNB_RPC);
 const providerAVA = new JsonRpcProvider(process.env.AVA_RPC);
-// const providerSepolia = new JsonRpcProvider(process.env.SEPOLIA_RPC);
 
 const contractBNB = new Contract(
   process.env.BRIDGE_CONTRACT_ADDRESS_BNB!,
@@ -24,131 +22,90 @@ const contractAVA = new Contract(
   providerAVA
 );
 
-// Define Redis connection for Bull queue
 const redisConfig = {
   redis: {
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: parseInt(process.env.REDIS_PORT || "6379", 10),
-    password:process.env.REDIS_PASSWORD || ''
+    password: process.env.REDIS_PASSWORD || "",
   },
 };
 const bridgeQueue = new Bull("bridgeQueue", redisConfig);
 
-// Define the contract ABI interface for log parsing
 const bridgeInterface = new Interface(ABI);
 
-// Define event listening function with `getLogs` and `parseLog`
-const listenToBridgeEventsBNB = async (
+const listenToBridgeEvents = async (
   provider: JsonRpcProvider,
   contract: Contract,
-  isBNB: boolean
+  network: "BNB" | "Avalanche"
 ) => {
-
-  const filter =  contract.filters.Bridge();
-    // console.log(filter);
-
   try {
-    const logs = await provider.getLogs({ ...filter, fromBlock: "latest" });    
+    let lastProcessedBlock = await prisma.networkStatus.findUnique({
+      where: { network },
+    });
+    const latestBlock = await provider.getBlockNumber();
 
-    logs.forEach((log) => {
-      
-      // const txhash = log.transactionHash
-      // console.log(txhash);
+    if (!lastProcessedBlock) {
+      const data = await prisma.networkStatus.create({
+        data: { network, lastProcessedBlock: latestBlock },
+      });
+      lastProcessedBlock = { ...data };
+    }
 
+    if (lastProcessedBlock.lastProcessedBlock >= latestBlock) {
+      return;
+    }
+
+    const filter = contract.filters.Bridge();
+    const logs = await provider.getLogs({
+      ...filter,
+      fromBlock: lastProcessedBlock.lastProcessedBlock + 1,
+      toBlock: "latest",
+    });
+
+    logs.forEach(async (log) => {
       const parsedLog = bridgeInterface.parseLog(log);
 
       if (parsedLog) {
-        const txhash = log.transactionHash
+        const txhash = log.transactionHash;
         const tokenAddress = parsedLog.args[0].toString();
         const amount = parsedLog.args[1].toString();
         const sender = parsedLog.args[2].toString();
 
-        console.log(`Bridge event detected: ${amount} tokens from ${sender} ,${txhash}`);
-
         bridgeQueue.add({
-          txhash:txhash.toLowerCase(),
-          tokenAddress: tokenAddress.toString(),
-          amount: amount.toString(),
-          sender: sender.toString(),
-          isBNB,
+          txhash: txhash.toLowerCase(),
+          tokenAddress: tokenAddress,
+          amount: amount,
+          sender: sender,
+          network,
         });
       }
     });
-  } catch (error) {
-    console.error("Error retrieving or parsing logs:", error);
-    return;
 
-  }
-};
-
-// Define event listening function with `getLogs` and `parseLog`
-const listenToBridgeEventsAVA = async (
-  provider: JsonRpcProvider,
-  contract: Contract,
-  isBNB: boolean
-) => {
-  // console.log("listenToBridgeEventsAVA",provider);
-
-  const filter = contract.filters.Bridge();
-  //   console.log(filter);
-
-  try {
-    const logs = await provider.getLogs({ ...filter, fromBlock: "latest" });
-
-    logs.forEach((log) => {
-      const parsedLog = bridgeInterface.parseLog(log);
-
-      if (parsedLog) {
-        // console.log(parsedLog.args);
-        const txhash = log.transactionHash
-        const tokenAddress = parsedLog.args[0].toString();
-        const amount = parsedLog.args[1].toString();
-        const sender = parsedLog.args[2].toString();
-
-        console.log(`Bridge event detected: ${amount} tokens from ${sender}`);
-
-        bridgeQueue.add({
-          txhash:txhash.toLowerCase(),
-          tokenAddress: tokenAddress.toString(),
-          amount: amount.toString(),
-          sender: sender.toString(),
-          isBNB,
-        });
-      }
+    await prisma.networkStatus.update({
+      where: { network },
+      data: { lastProcessedBlock: latestBlock },
     });
+    
   } catch (error) {
-    console.error("Error retrieving or parsing logs:", error);
-    return;
+    console.error(`Error retrieving or parsing logs on ${network}:`, error);
   }
 };
 
 // Set up polling listeners
+setInterval(() => listenToBridgeEvents(providerBNB, contractBNB, "BNB"), 5000);
 setInterval(
-  () => listenToBridgeEventsBNB(providerBNB, contractBNB, false),
-  1000
-); // 10 sec polling
-setInterval(
-  () => listenToBridgeEventsAVA(providerAVA, contractAVA, true),
-  1000
-); // 10 sec polling
+  () => listenToBridgeEvents(providerAVA, contractAVA, "Avalanche"),
+  5000
+);
 
-// Process jobs in bridgeQueue
 bridgeQueue.process(async (job) => {
-  const { txhash,tokenAddress, amount, sender, isBNB } = job.data;
-
-
-
-  console.log(
-    `Processing bridge event for ${amount} tokens from ${sender} ${tokenAddress} ${isBNB}`
-  );
+  const { txhash, tokenAddress, amount, sender, network } = job.data;
 
   try {
-
     let transaction = await prisma.transactionData.findUnique({
       where: { txHash: txhash },
     });
 
-    // If transaction does not exist, create it
     if (!transaction) {
       transaction = await prisma.transactionData.create({
         data: {
@@ -156,22 +113,22 @@ bridgeQueue.process(async (job) => {
           tokenAddress,
           amount,
           sender,
-          network: isBNB ? 'BNB' : 'Avalanche',
+          network,
           isDone: false,
         },
       });
     }
 
     if (transaction.isDone) {
-      console.log(`Transaction ${txhash} is already processed. Skipping.`);
-      return { success: true, message: 'Transaction already processed' };
+      return { success: true, message: "Transaction already processed" };
     }
 
-    await transferToken(isBNB, tokenAddress, amount, sender);
+    await transferToken(network === "BNB", tokenAddress, amount, sender);
     await prisma.transactionData.update({
       where: { txHash: txhash },
       data: { isDone: true },
     });
+
     return { success: true };
   } catch (error) {
     console.error(`Error processing job ${job.id}:`, error);
@@ -179,7 +136,6 @@ bridgeQueue.process(async (job) => {
   }
 });
 
-// Function to transfer token on respective chain
 const transferToken = async (
   isBNB: boolean,
   tokenAddress: string,
@@ -187,9 +143,9 @@ const transferToken = async (
   sender: string
 ) => {
   try {
-    const RPC = isBNB ? process.env.BNB_RPC : process.env.AVA_RPC;
+    const RPC = !isBNB ? process.env.BNB_RPC : process.env.AVA_RPC;
     const pk = process.env.PK!;
-    const contractAddress = isBNB
+    const contractAddress = !isBNB
       ? process.env.BRIDGE_CONTRACT_ADDRESS_BNB!
       : process.env.BRIDGE_CONTRACT_ADDRESS_AVA!;
 
@@ -197,16 +153,14 @@ const transferToken = async (
     const wallet = new Wallet(pk, provider);
     const contractInstance = new Contract(contractAddress, ABI, wallet);
 
-    const testToken = isBNB
+    const testToken = !isBNB
       ? process.env.TESTTOKEN_BNB!
       : process.env.TESTTOKEN_AVA!;
 
     const tx = await contractInstance.redeem(testToken, sender, amount);
     await tx.wait();
-    console.log(`Successfully transferred ${amount} tokens to ${sender}`);
 
   } catch (error) {
-    console.log("Transfer token error:", error);
     throw error;
   }
 };
